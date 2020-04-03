d789d9fd8c06a54557ea56e1e1e5c35da48dbbd0
Gerardo Enrique Alvarenga <alva.92@outlook.com>
2020-04-03 17:13:23 -0400

    Initial rough implementation and pseudo code

--- metronome/src/metronome.cpp
+++ metronome/src/metronome.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/iofunc.h>
+#include <sys/dispatch.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
 
+#define ATTACH_POINT    "metronome"
+#define METRONOME_PULSE_CODE _PULSE_CODE_MINAVAIL
+#define PAUSE_PULSE_CODE 7
+#define QUIT_PULSE_CODE  8
+
+typedef union
+{
+	struct _pulse   pulse;
+	char msg[255];
+
+} my_message_t;
+
+name_attach_t *attach;
+int rcvid;
+char data[255];
 int metronome_coid;
+my_message_t msg;
 
 /**
  * This function "drives" the metronome. It receives pulse from interval timer;
@@ -9,7 +32,78 @@ int metronome_coid;
  */
 void metronome_thread()
 {
+		/* Phase I - create a named channel to receive pulses */
 
+	/* Create a local name to register the device */
+	if ((attach = name_attach(NULL, ATTACH_POINT, 0)) == NULL)
+	{
+		printf("Failed to attach to device");
+	}
+
+	/*
+	 *  TODO
+	 *  calculate the seconds-per-beat and nano seconds for the interval timer
+	 *  create an interval timer to "drive" the metronome
+	 *  configure the interval timer to send a pulse to channel at attach when it expires
+	 */
+
+		/* Phase II - receive pulses from interval timer OR io_write(pause, quit) */
+	while(1)
+	{
+	   rcvid = MsgReceivePulse(attach->chid, &msg, sizeof(msg), NULL);
+
+	   if (rcvid == -1) /* Error condition, exit */
+	   {
+		   printf("Failed to receive message");
+		   break;
+	   }
+
+
+	   if (rcvid == 0) /* Pulse received */
+	   {
+		   switch (msg.pulse.code)
+		   {
+			   case METRONOME_PULSE_CODE:
+
+			   	   /*
+			   	    * TODO
+			   	    * display the beat to stdout
+			   	    * must handle 3 cases:
+			   	    * start-of-measure: |1
+			   	    * mid-measure: the symbol, as seen in the column "Pattern for Intervals within Each Beat"
+			   	    * end-of-measure: \n
+			   	    */
+
+				   break;
+
+		       case PAUSE_PULSE_CODE:
+
+		    	   /*
+		    	    * TODO:
+		    	    * pause the running timer for pause <int> seconds
+		    	    */
+
+			   	   break;
+		       case QUIT_PULSE_CODE:
+
+		    	   /*
+		    	    * TODO:
+		    	    * implement Phase III:
+		    	    * delete interval timer
+		    	    * call name_detach()
+		    	    * call name_close()
+		    	    * exit with SUCCESS
+		    	    */
+
+			   	   break;
+			   default:
+				   printf("Not the expected code");
+				   break;
+			   }
+		   continue;
+	   }
+
+	}
 }
 
 int io_read(resmgr_context_t *ctp, io_read_t *msg, RESMGR_OCB_T *ocb)
@@ -43,6 +137,56 @@ int io_read(resmgr_context_t *ctp, io_read_t *msg, RESMGR_OCB_T *ocb)
 	return(_RESMGR_NPARTS(1));
 }
 
+int io_write(resmgr_context_t *ctp, io_write_t *msg, RESMGR_OCB_T *ocb)
+{
+    int nb = 0;
+
+    if( msg->i.nbytes == ctp->info.msglen - (ctp->offset + sizeof(*msg) ))
+    {
+		char *buf;
+		char *pause_msg;
+		int i;
+		int pause;
+		buf = (char *)(msg+1);
+
+		std::cout << "Buffer: \n" << buf << std::endl;
+		std::cout.flush();
+
+		if(strstr(buf, "pause") != NULL)
+		{
+			for(i = 0; i < 2; i++){
+				pause_msg = strsep(&buf, " ");
+			}
+			pause = atoi(pause_msg);
+			if(pause >= 1 && pause <= 10)
+			{
+				MsgSendPulse(metronome_coid, SchedGet(0,0,NULL), PAUSE_PULSE_CODE, pause);
+			}
+			else
+			{
+				printf("Pause needs to be a number between 1 and 10. \n");
+			}
+		}
+		else if (strstr(buf, "quit") != NULL)
+		{
+			MsgSendPulse(metronome_coid, SchedGet(0,0,NULL), QUIT_PULSE_CODE, 0);
+		}
+		else
+		{
+			strcpy(data, buf);
+		}
+
+		nb = msg->i.nbytes;
+    }
+
+    _IO_SET_WRITE_NBYTES (ctp, nb);
+
+    if (msg->i.nbytes > 0)
+        ocb->attr->flags |= IOFUNC_ATTR_MTIME | IOFUNC_ATTR_CTIME;
+
+    return (_RESMGR_NPARTS (0));
+}
+
 int io_open(resmgr_context_t *ctp, io_open_t *msg, RESMGR_HANDLE_T *handle, void *extra)
 {
     if ((metronome_coid = name_open( "metronome", 0 )) == -1) {
@@ -69,7 +213,49 @@ int main(int argc, char *argv[])
 	 *   time-signature (bottom)
 	 */
 
+	dispatch_t* dpp;
+	resmgr_io_funcs_t io_funcs;
+	resmgr_connect_funcs_t connect_funcs;
+	iofunc_attr_t ioattr;
+	dispatch_context_t   *ctp;
+	int id;
+
+	if ((dpp = dispatch_create ()) == NULL)
+	{
+		fprintf (stderr,
+				"%s:  Unable to allocate dispatch context.\n",
+				argv [0]);
+
+		return EXIT_FAILURE;
+	}
+
+	iofunc_func_init(_RESMGR_CONNECT_NFUNCS, &connect_funcs, _RESMGR_IO_NFUNCS, &io_funcs);
+	connect_funcs.open = io_open;
+	io_funcs.read = io_read;
+	io_funcs.write = io_write;
+
+	iofunc_attr_init(&ioattr, S_IFCHR | 0666, NULL, NULL);
+
+	if ((id = resmgr_attach (dpp, NULL, "/dev/local/metronome",
+					_FTYPE_ANY, 0, &connect_funcs, &io_funcs,
+	                &ioattr)) == -1)
+	{
+
+		fprintf (stderr,
+				"%s:  Unable to attach name.\n",
+				argv [0]);
+
+		return EXIT_FAILURE;
+	}
+
 	// TODO: Create the metronome thread
 
-	return 0;
+	ctp = dispatch_context_alloc(dpp);
+
+	while(1)
+	{
+		ctp = dispatch_block(ctp);
+		dispatch_handler(ctp);
+	}
+	return EXIT_SUCCESS;
 }
